13067a98af9028c13665d7bec0173a02c09b6a9b
gerard <gerard@gerard-K55A>
2014-01-17 15:54:54 +0100

    M5

--- M5Compartit/src/exemple/Coxe.java
+++ M5Compartit/src/exemple/Coxe.java
@@ -6,6 +6,15 @@ public class Coxe {
 	public Coxe(int matricula) {
 		this.matricula = matricula;
 	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	
 	
 	
 }
